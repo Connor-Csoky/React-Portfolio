27ae9a8fdd47a8c869b1443d6d20383199a78a02
Connor-Csoky <connorcsoky@gmail.com>
2023-08-24 14:42:22 -0400

    contact working
    contact working

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,31 +1,48 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./Contact.css";
 
 const Contact = () => {
-
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('', '', form.current, '')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="formHolder">
+      <form className="form" ref={form} onSubmit={sendEmail}>
+        <div>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email" name="user_email" />
+        </div>
+        <div>
+          <label>Message</label>
+          <textarea name="message" />
+        </div>
+        <input type="submit" value="Send" />
+      </form>
+    </div>
   );
 };
 
