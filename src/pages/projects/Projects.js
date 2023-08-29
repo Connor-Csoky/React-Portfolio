0d38959ec2520763456c24cd1d3debf156ff0dd3
Connor-Csoky <connorcsoky@gmail.com>
2023-08-29 16:26:39 -0400

    Update Projects.js
    changes

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -5,24 +5,34 @@ import RoboFriends from "./robofriends.png";
 import { useState } from "react";
 
 export default function Projects() {
-
   return (
     <div id="projects" className="projects">
       <div className="project-container ">
         <div className="project-text-container">
           <h1>RoboFriends</h1>
-          <p>My first website using react. A project assigned through "ZeroToMastery" web development course. Essential in helping build my career as a web developer and develop upon my skills.</p>
+          <p>
+            My first website using react. A project assigned through
+            "ZeroToMastery" web development course. Essential in helping build
+            my career as a web developer and develop upon my skills.
+          </p>
           <button>View</button>
         </div>
-        <img className="project-image" src={Arcade} alt="arcade" />
+        <img className="project-image" src={RoboFriends} alt="arcade" />
       </div>
 
       <div className="project-container project1">
-        <img className="project-image" src={RoboFriends} alt="arcade" />
+        <img className="project-image" src={Arcade} alt="arcade" />
         <div className="project-text-container">
           <h1>Arcade</h1>
-          <p>A small arcade made up of games that I created. I created the different games to test features such as javascripts canvas, and animations using html and css. This project taught me many valuable lessons about web development.</p>
-          <a href="https://connor-csoky.github.io/Coding-Projects/"><button>View</button></a> 
+          <p>
+            A small arcade made up of games that I created. I created the
+            different games to test features such as javascripts canvas, and
+            animations using html and css. This project taught me many valuable
+            lessons about web development.
+          </p>
+          <a href="https://connor-csoky.github.io/Coding-Projects/">
+            <button>View</button>
+          </a>
         </div>
       </div>
     </div>
